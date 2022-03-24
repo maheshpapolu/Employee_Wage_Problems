fb4f807f3ea37215d36aa393aa323f5295fde82c
maheshpapolu <maheshnaidu9666@gmail.com>
2022-03-25 00:13:43 +0530

    added  Calculating_Wages_for_a_Month.files

---
+++ Calculating_Wages_for_a_Month/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculating_Wages_for_a_Month
+{
+    internal class Program
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_DATE = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        static void Main(string[] args)
+        {
+            int empHrs = 0, empWage = 0, totalEmpWage = 0;
+
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0,3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_DATE:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage : " + totalEmpWage);
+            }
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+
+
+        }
+    }
+}
